1cf3ea8025d1a2c6927044f486714a48edbb7b76
Adam Kaźmierski <adam.kazmierski@britenet.com.pl>
2019-01-05 14:38:50 +0100

    Funkcje energi i aktywacji

--- johnnewcombe-hopfield-network-80fe0d4275f6/ZmsiProjOne/Program.cs
+++ johnnewcombe-hopfield-network-80fe0d4275f6/ZmsiProjOne/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DMU.Math;
 using System;
+using System.Collections.Generic;
 
 namespace ZmsiProjOne
 {
@@ -17,5 +18,81 @@ namespace ZmsiProjOne
         {
 
         }
+
+
+        double EnergiaSync(Matrix w,Matrix I,Matrix x)
+        {
+            double suma = 0;
+            int n = w.RowCount;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    suma += w.GetElement(i, j) * x.GetElement(i, 1) * x.GetElement(j, 0);
+                }
+            }
+            suma *= -1;
+
+            for (int i = 0; i < n; i++)
+            {
+                suma+=I.GetElement(i, 0) * (x.GetElement(i, 1) + x.GetElement(i, 0));
+            }
+
+            return suma;
+
+        }
+
+
+        double EnergiaAsync(Matrix w,Matrix x)
+        {
+            double suma = 0;
+            int n = w.RowCount;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    suma += w.GetElement(i, j) * x.GetElement(i, 0) * x.GetElement(j, 0);
+                }
+
+            }
+
+
+            suma *= -0.5;
+
+            return suma;
+        }
+
+
+        double RoznicaEnergiSync(List<double> e)
+        {
+            double roznica = 0;
+
+            int liczbaElementow= e.Count;
+
+            roznica = e[liczbaElementow - 1] - e[liczbaElementow - 2];
+            return roznica;
+
+        }
+
+        int FunkcjaAktywacjiPolarna(double element)
+        {
+            if(element <=0)
+                return 0;
+            else
+                return 1;
+        }
+
+        int FunkcjaAktywacjiBiPolarna(double element)
+        {
+            if (element <= 0)
+                return -1;
+            else
+                return 1;   
+        }
+
+
     }
 }
