7382f8c847b237297c8bfb1389d3e94e7ed32c2b
adamkazmiers <adamkazmiers@gmail.com>
2019-01-05 13:51:48 +0100

    Init

---
+++ johnnewcombe-hopfield-network-80fe0d4275f6/ZmsiProjOne/Program.cs
@@ -0,0 +1,21 @@
+﻿using DMU.Math;
+using System;
+
+namespace ZmsiProjOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+
+            Console.ReadKey();
+        }
+
+        void SynchHopfield(Matrix matrix)
+        {
+
+        }
+    }
+}
