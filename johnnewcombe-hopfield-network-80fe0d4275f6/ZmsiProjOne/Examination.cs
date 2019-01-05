26d98a92d1a6e3156c29b797e3b8b81ad9636093
adamkazmiers <adamkazmiers@gmail.com>
2019-01-05 17:11:25 +0100

    rew

---
+++ johnnewcombe-hopfield-network-80fe0d4275f6/ZmsiProjOne/Examination.cs
@@ -0,0 +1,12 @@
+﻿using DMU.Math;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZmsiProjOne
+{
+    public class Examination
+    {
+        public Matrix PotencjalWejsciowy { get; set; }
+    }
+}
---
+++ johnnewcombe-hopfield-network-80fe0d4275f6/ZmsiProjOne/ExaminationStep.cs
@@ -0,0 +1,16 @@
+﻿using DMU.Math;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZmsiProjOne
+{
+    public class ExaminationStep
+    {
+        public int Numer { get; set; }
+        public Matrix PotencjalWejsciowy { get; set; }
+        public Matrix ObliczonyPotencjalWejsciowy { get; set; }
+        public Matrix PotencjalWyjsciowy { get; set; }
+        public double Energia { get; set; }
+    }
+}
